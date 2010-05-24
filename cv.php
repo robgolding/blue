1cf17cf8a541a97ac32eb7ee884df865f7b8c743
Rob Golding <rob@robgolding.com>
2010-05-24 19:48:31 +0100

    Added title to CV page

--- cv.php
+++ cv.php
@@ -6,17 +6,14 @@ Template Name: CV
 
 <?php get_header(); ?>
 	<div class="container_12">
-	
 		<?php if (have_posts()) : ?>
-		
 			<?php while (have_posts()) : the_post(); ?>
 				<section class="grid_12">
-					<?php the_content('Read the rest of this entry &raquo;'); ?>
+					<h1><?php the_title(); ?></h1>
+					<?php the_content(''); ?>
 				</section>
 			<?php endwhile; ?>
-		
 		<?php endif; ?>
-	
 	</div><!-- /container_12 -->
 
 <?php get_footer(); ?>
