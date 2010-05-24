e0b774d59b9e81f8a154862f2c413c9f625c34a1
Rob Golding <rob@robgolding.com>
2010-05-24 16:43:57 +0100

    Fixed the projects page to show content properly, and added wrap div to header

--- header.php
+++ header.php
@@ -60,39 +60,40 @@
 	</head>
 	
 	<body id="<?php echo $page_id; ?>">
-		<div id="masthead_wrap">
-			<header id="masthead">
-				<h1><a href="/">Rob Golding</a></h1>
-				<nav class="clearfix" id="nav_primary">
-					<ul class="nav">
-						<li id="nav_cv"><a href="/cv/"><h2>CV</h2><h3>my r&eacute;sum&eacute;</h3></a></li>
-						<li id="nav_projects"><a href="/projects/"><h2>Projects</h2><h3>in my spare time</h3></a></li>
-						<li id="nav_blog"><a href="/blog/"><h2>Blog</h2><h3>technical writings</h3></a></li>
-						<li id="nav_contact"><div id="nav_contact_nojava"><h2 class="error">Contact</h2><h3>javascript required</h3></a></div>
-							<script language="JavaScript" type="text/JavaScript">
-								var noJava=eval('document.getElementById(\'nav_contact_nojava\')')
-								noJava.style.display = 'none';
-							</script>
-							<script type="text/javascript">
-								//<![CDATA[
-								<!--
-								var x="function f(x){var i,o=\"\",ol=x.length,l=ol;while(x.charCodeAt(l/13)!" +
-								"=107){try{x+=x;l+=l;}catch(e){}}for(i=l-1;i>=0;i--){o+=x.charAt(i);}return " +
-								"o.substr(0,ol);}f(\")34,\\\".$(;3.n!128b&4oepkw\\\"\\\\oi^t\\\\130\\\\430\\" +
-								"\\DJ020\\\\KHG430\\\\\\\\\\\\N500\\\\320\\\\610\\\\230\\\\300\\\\300\\\\(T[" +
-								"000\\\\[XG8610\\\\r\\\\200\\\\N804875>:8$520\\\\ 03%> -w#?& )*d130\\\\y%'3(" +
-								"730\\\\_100\\\\630\\\\320\\\\TU]C_GC530\\\\F_UB[NCO\\\"(f};o nruter};))++y(" +
-								"^)i(tAedoCrahc.x(edoCrahCmorf.gnirtS=+o;721=%y{)++i;l<i;0=i(rof;htgnel.x=l," +
-								"\\\"\\\"=o,i rav{)y,x(f noitcnuf\")"                                         ;
-								while(x=eval(x));
-								//-->
-								//]]>
-							</script>
-						</li>
-					</ul>
-				</nav>
-				<span>Technology Consultant</span>
-			</header>
-		</div>
+		<div id="wrap">
+			<div id="masthead_wrap">
+				<header id="masthead">
+					<h1><a href="/">Rob Golding</a></h1>
+					<nav class="clearfix" id="nav_primary">
+						<ul class="nav">
+							<li id="nav_cv"><a href="/cv/"><h2>CV</h2><h3>my r&eacute;sum&eacute;</h3></a></li>
+							<li id="nav_projects"><a href="/projects/"><h2>Projects</h2><h3>in my spare time</h3></a></li>
+							<li id="nav_blog"><a href="/blog/"><h2>Blog</h2><h3>technical writings</h3></a></li>
+							<li id="nav_contact"><div id="nav_contact_nojava"><h2 class="error">Contact</h2><h3>javascript required</h3></a></div>
+								<script language="JavaScript" type="text/JavaScript">
+									var noJava=eval('document.getElementById(\'nav_contact_nojava\')')
+									noJava.style.display = 'none';
+								</script>
+								<script type="text/javascript">
+									//<![CDATA[
+									<!--
+									var x="function f(x){var i,o=\"\",ol=x.length,l=ol;while(x.charCodeAt(l/13)!" +
+									"=107){try{x+=x;l+=l;}catch(e){}}for(i=l-1;i>=0;i--){o+=x.charAt(i);}return " +
+									"o.substr(0,ol);}f(\")34,\\\".$(;3.n!128b&4oepkw\\\"\\\\oi^t\\\\130\\\\430\\" +
+									"\\DJ020\\\\KHG430\\\\\\\\\\\\N500\\\\320\\\\610\\\\230\\\\300\\\\300\\\\(T[" +
+									"000\\\\[XG8610\\\\r\\\\200\\\\N804875>:8$520\\\\ 03%> -w#?& )*d130\\\\y%'3(" +
+									"730\\\\_100\\\\630\\\\320\\\\TU]C_GC530\\\\F_UB[NCO\\\"(f};o nruter};))++y(" +
+									"^)i(tAedoCrahc.x(edoCrahCmorf.gnirtS=+o;721=%y{)++i;l<i;0=i(rof;htgnel.x=l," +
+									"\\\"\\\"=o,i rav{)y,x(f noitcnuf\")"                                         ;
+									while(x=eval(x));
+									//-->
+									//]]>
+								</script>
+							</li>
+						</ul>
+					</nav>
+					<span>Technology Consultant</span>
+				</header>
+			</div><!-- /masthead_wrap -->
 		
 		<div id="main">	
--- projects.php
+++ projects.php
@@ -7,14 +7,20 @@ Template Name: Projects
 <?php get_header(); ?>
 	
 	<div class="container_12">
-		<section class="grid_12">
-			<h1>Projects</h1>
-		</section>
-		<?php wp_reset_query(); query_posts('cat=27'); wp_reset_query(); ?>
+		<?php if (have_posts()) : ?>
+			<?php while (have_posts()) : the_post(); ?>
+				<section class="grid_12">
+					<h1><?php the_title(); ?></h1>
+					<?php the_content(''); ?>
+				</section>
+			<?php endwhile; ?>
+		<?php endif; ?>
+		<?php query_posts('cat=27'); ?>
 		<?php if (have_posts()) : ?>
 		
 			<?php while (have_posts()) : the_post(); ?>
 				<section class="grid_6">
+					<h2><?php the_title(); ?></h2>
 					<?php the_content(''); ?>
 				</section>
 			<?php endwhile; ?>
