cdcd1700e86fdb866952d84e16b9255f14ef4f03
Rob Golding <rob@robgolding.com>
2010-05-25 10:53:50 +0100

    Fixed index template to use one long <section>, so sidebar works properly

--- index.php
+++ index.php
@@ -3,30 +3,23 @@
 	<div class="container_12">
 		<?php if (have_posts()) : ?>
 			
-			<?php $first = true; ?>
-			
-			<?php while (have_posts()) : the_post(); ?>
-				<section class="grid_8 post" id="post-<?php the_ID(); ?>">
-					<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-					<p class="meta"><?php the_time('F jS, Y') ?> by <strong><?php the_author() ?></strong></p>
+			<section class="grid_8 post">
+				<?php while (have_posts()) : the_post(); ?>
+					<div id="post-<?php the_ID(); ?>">
+						<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+						<p class="meta"><?php the_time('F jS, Y') ?> by <strong><?php the_author() ?></strong></p>
 
-					<?php the_content('Read the rest of this entry &raquo;'); ?>
+						<?php the_content('Read the rest of this entry &rarr;'); ?>
 
-					<p><?php comments_popup_link('No Comments', '1 Comment &rarr;', '% Comments &rarr;'); ?></p>
-				</section>
-				<?php if ($first) : $first = false; ?>
-					<section id="sidebar" class="grid_4">
-						<?php get_sidebar(); ?>
-					</section><!-- /sidebar -->
-				<?php endif; ?>
-			<?php endwhile; ?>
-			
-			<section class="grid_8">
+						<p><?php comments_popup_link('No Comments', '1 Comment &rarr;', '% Comments &rarr;'); ?></p>
+					</div>
+				<?php endwhile; ?>
+				
 				<p>
-					<?php next_posts_link('&laquo; Older Entries') ?> <?php previous_posts_link('Newer Entries &raquo;') ?>
+					<?php next_posts_link('&larr; Older Entries') ?>
+					<?php previous_posts_link('Newer Entries &rarr;') ?>
 				</p>
 			</section>
-		
 		<?php else : ?>
 			<section class="grid_8">
 				<h1>Not Found</h1>
@@ -34,7 +27,9 @@
 					<?php include (TEMPLATEPATH . "/searchform.php"); ?>
 			</section>
 		<?php endif; ?>
-		
+		<section id="sidebar" class="grid_4">
+			<?php get_sidebar(); ?>
+		</section><!-- /sidebar -->
 	</div><!-- /container_12 -->
 
 <?php get_footer(); ?>
